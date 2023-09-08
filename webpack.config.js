b2e148e27b6e803eb59321333cfe465894193cfe
Natalia Platova <nataliaplatova47@gmail.com>
2023-09-08 17:50:10 +0300

    Setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: ["@babel/polyfill", "./src/index.tsx"],
+    resolve: {
+        extensions: [".tsx",".ts",".js"],
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].[hash].js"
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: "./src/index.html"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(jpeg|jpg|png|svg)$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(ts|js)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+            {
+                test: /\.(tsx|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-react', '@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    }
+}
