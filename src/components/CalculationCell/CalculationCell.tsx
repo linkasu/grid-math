1e137ca71fdc60a6c00a804bf4ed53c9afbecd20
Natalia Platova <nataliaplatova47@gmail.com>
2023-10-18 10:54:28 +0300

    temporarely removed autofocus after input

--- src/components/CalculationCell/CalculationCell.tsx
+++ src/components/CalculationCell/CalculationCell.tsx
@@ -14,7 +14,7 @@ const CalculationCell = (props: ICalculationCellProps) => {
     const { rowType, isOffsetCell = false, isFocused = false, onCellEnter, focusNextCell } = props;
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const maxLength = rowType === "helper" ? 3 : 1;
+    const maxLength = rowType === "helper" ? 2 : 1;
 
     useEffect(() => {
         if (isFocused) {
@@ -26,13 +26,17 @@ const CalculationCell = (props: ICalculationCellProps) => {
     const onInput = () => {
         const value = inputRef.current?.value;
         if (!value || !value.trim()) return;
-        if (isCorrectValue(value)) {
-            if (rowType === "helper") return;
-            focusNextCell(rowType === "number" ? "right" : "left");
-        } else {
+        if (!isCorrectValue(value)) {
             /* @ts-ignore*/
             inputRef.current?.value = value.slice(0, -1);
         }
+        /*if (isCorrectValue(value)) {               Uncomment if autofocus after input needed
+            if (rowType === "helper") return;
+            focusNextCell(rowType === "number" ? "right" : "left");
+        } else {
+            /* @ts-ignore
+            inputRef.current?.value = value.slice(0, -1);
+        }*/
     };
     const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "ArrowRight") {
