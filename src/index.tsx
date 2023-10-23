7668a7be8599d118dc5e2f6a3044ec1230cd82da
Ivan Bakaidov <ibakaidov@ya.ru>
2023-10-23 13:16:03 +0300

    add meta information

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import "./index.scss";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import './assets/social.png'
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
